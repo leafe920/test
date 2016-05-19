5ebb6382fb48539d5ba23868026efe3ea0bc1bfb
leafe.qian <leafe.qian@sh301030101002.99bill.com>
2016-05-19 17:55:19 +0800

    Signed-off-by: leafe.qian <leafe.qian@sh301030101002.99bill.com>

--- src/githubdemo/TEST.java
+++ src/githubdemo/TEST.java
@@ -1,13 +1,8 @@
 package githubdemo;
 
 /**
-<<<<<<< HEAD
- * tes321312t
- * @author c11111c
-=======
- * te32t123123
- * @author ee
->>>>>>> branch 'master' of git@github.com:leafe920/test.git
+  *  testtst231321
+ * git@github.com:leafe920/test.git
  * @date 2016年5月19日 下午5:19:28
  */
 public class TEST {
